84f9b2929b6ca50f8ddc734b98cf1997ab59b4bd
felixhelgesson <felix.helgesson@gmail.com>
2017-04-25 13:09:20 +0200

    Småfix
    Småfix

--- Life of ball/Assets/Scripts/Player/PlayerMovement.cs
+++ Life of ball/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,44 +7,39 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 6f;
     public float jumpSpeed = 6f;
+    public Rigidbody Level;
 
     private Vector3 movement;
     Rigidbody playerRigidbody;
+
     float f;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        
     }
 
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        
+     
+
+         Move(h, 0, v);
+    }
+
+    void Move(float h, float j, float v)
+    {
 
         movement = new Vector3(h, 0, v);
         playerRigidbody.AddForce(movement * speed);
 
-        if (Input.GetAxis("Jump") > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && )
         {
-            playerRigidbody.AddForce(Vector3.up * jumpSpeed);
+            movement.Set(h, jumpSpeed, v);
+            playerRigidbody.AddForce(movement);
         }
-
-        // Move(h, 0, v);
     }
-
-    //void Move(float h, float j, float v)
-    //{
-    //    movement.Set(h, j, v);
-    //    //movement = movement.normalized * speed * Time.deltaTime;
-
-
-    //    //playerRigidbody.MovePosition(transform.position + movement);
-
-
-    //    if (Input.GetAxisRaw("Jump") > 0)
-    //    {
-    //        playerRigidbody.AddForce(Vector3.up * jumpSpeed);
-    //    }
-    //}
 }
