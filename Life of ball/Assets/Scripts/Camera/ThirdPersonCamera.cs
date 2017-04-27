2a4e930d87188fe295cfd465d76506aef995753c
Tomdozz <tom_1995@msn.com>
2017-04-27 14:52:39 +0200

    #3 Camera rotation
    Camera is now rotation

--- Life of ball/Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Life of ball/Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
-
+public class ThirdPersonCamera : MonoBehaviour
+{
     public GameObject player;
     private Vector3 offset;
 
     public Transform lookAt;
     public Transform camTransform;
+     
+    float Y_ANGLE_MAX = 50.0f;
+    float Y_ANGLE_MIN = 0.0f;
 
     private Camera cam;
     float distance = 10.0f;
@@ -25,16 +28,23 @@ public class CameraFollow : MonoBehaviour {
         offset = transform.position - player.transform.position;
     }
 
+    void Update()
+    {
+        currentY += Input.GetAxis("Mouse X");
+        currentX += Input.GetAxis("Mouse Y");
+        //currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         Vector3 dir = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentX, currentY,0);
+        Quaternion rotation = Quaternion.Euler(currentX, currentY, 0);
         camTransform.position = lookAt.position + rotation * dir;
-
+        camTransform.LookAt(lookAt.position);
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         //transform.position = player.transform.position + offset;
-       
+
         // transform.LookAt(player.transform);
     }
 }
