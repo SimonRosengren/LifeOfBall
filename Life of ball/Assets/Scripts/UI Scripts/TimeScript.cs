1aaec2e70bf0abd89536cfa1dd3ec29629cf01ba
Tomdozz <tom_1995@msn.com>
2017-04-29 16:42:31 +0200

    #5 Timer
    We have to decide if i should count up or down

--- Life of ball/Assets/Scripts/UI Scripts/TimeScript.cs
+++ Life of ball/Assets/Scripts/UI Scripts/TimeScript.cs
@@ -2,20 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeScript : MonoBehaviour {
 
-    public string ellapsedTime; 
+    public string ellapsedTime;
+    Text timeText; 
     DateTime time;
     float gameTime;
 	// Use this for initialization
 	void Start () {
-        gameTime += time.Second;
-        ellapsedTime = gameTime.ToString();
+        timeText = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        gameTime += Time.deltaTime;
+        ellapsedTime = gameTime.ToString();
+        int timer = (int)gameTime;
+        timeText.text = "Time left: "+timer.ToString();//ellapsedTime;
 		
 	}
 }
